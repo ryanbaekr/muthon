ad97b5a28ea30672ddc5353d45698b074b6cf297
Stephen Szemis <sszemis@eaglecondev.com>
2024-09-28 19:36:06 -0400

    Actual first step on the road to progress

--- muthon/sca.py
+++ muthon/sca.py
@@ -3,7 +3,7 @@
 from typing import Optional, TypedDict, Unpack
 
 import libcst as cst
-
+from libcst.metadata import ScopeProvider, PositionProvider
 
 class KeyWordArgs(TypedDict):
     module: Optional[str]
@@ -12,11 +12,34 @@ class KeyWordArgs(TypedDict):
     verbose: bool
 
 
+class muthonVisitor(cst.CSTVisitor):
+    METADATA_DEPENDENCIES = (ScopeProvider, PositionProvider)
+
+    def visit_Call(self, node: cst.Call) -> None:
+        # Get the scope of the call
+        scope = self.get_metadata(ScopeProvider, node)
+        for arg in node.args:
+            if isinstance(arg.value, cst.Name):
+                var_name: str = arg.value.value
+                if scope is None:
+                    continue
+                assigns = scope[var_name]
+                # TODO check type of assignment
+                for assign in assigns:
+                    if not (isinstance(assign, cst.metadata.Assignment) and isinstance(assign.node, cst.Name)):
+                        continue
+                    # get position data from assign
+                    pos = self.get_metadata(PositionProvider, assign.node)
+                    print(f"{assign.node.value} found at line {pos.start.line}, column {pos.start.column}")
+
 def parse_file(file: str) -> Optional[cst.Module]:
     """Parse syntax tree of individual file"""
     with open(file) as f:
         try:
             source_tree = cst.parse_module(f.read())
+            wrapper = cst.MetadataWrapper(source_tree)
+            result = wrapper.visit(muthonVisitor())
+            #print(result)
         except cst.ParserSyntaxError as syntax:
             print(syntax)
             source_tree = None
@@ -32,7 +55,6 @@ def run_sca(**kwargs: Unpack[KeyWordArgs]) -> bool:
     print(sources)
     for file in sources:
         print(parse_file(file))
-
 
     print(verbose)
 
--- tests/test_process_args.py
+++ tests/test_process_args.py
@@ -1,7 +1,10 @@
 """Testing Argument Processing"""
 
+import os
+
 from muthon.sca import process_args
 from muthon.sca import KeyWordArgs
+from muthon.sca import run_sca
 
 def test_module_only() -> None:
     """Test process_args with only a module arg"""
@@ -19,3 +22,18 @@ def test_module_only() -> None:
     assert sources[0] == "module.py"
 
     assert verbose is False
+
+
+def test_sca_only() -> None:
+    """Test run_sca with only a module arg"""
+
+    kwargs: KeyWordArgs = {
+        "module": os.path.realpath("tests/fixtures/mutable_in.py"),
+        "package": None,
+        "exclude": None,
+        "verbose": False,
+    }
+
+    result = run_sca(**kwargs)
+
+    assert result
